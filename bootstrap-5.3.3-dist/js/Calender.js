31f5a4a8840f69bcea4e4be7a4d3ea6d3acb74e7
SASEOSU <ohiostate@saseconnect.org>
2025-07-11 11:15:06 +0900

    Update Calender.js (bigger letters top of calendar)

--- bootstrap-5.3.3-dist/js/Calender.js
+++ bootstrap-5.3.3-dist/js/Calender.js
@@ -326,6 +326,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("Countdown").innerHTML = "Time for the Diwali Celebration!";
+    document.getElementById("Countdown").innerHTML = "Time for Scoops N' Science!";
   }
 }, 1000);
