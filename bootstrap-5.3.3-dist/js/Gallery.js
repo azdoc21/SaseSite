9cbdc89c52b950eb6f1945376ec3e7dc4c2e161a
SASEOSU <ohiostate@saseconnect.org>
2025-07-12 20:54:11 +0900

    Update Gallery.js
    added an underscore

--- bootstrap-5.3.3-dist/js/Gallery.js
+++ bootstrap-5.3.3-dist/js/Gallery.js
@@ -129,7 +129,7 @@ class GalleryManager {
         const eventImages = [];
         if (event['Num of Pics'] > 0) {
             for (let i = 1; i <= event['Num of Pics']; i++) {
-                eventImages.push(`../images/Gallery/${event.Name}${i}.png`);
+                eventImages.push(`../images/Gallery/${event.Name}$_{i}.png`);
             }
         } else {
             eventImages.push('../images/sase_logo.png');
